5d4dbc473830b8dfc8124d95788cf81006b5ddb9
Ricky Medina <rickmedina85@gmail.com>
2022-11-16 13:56:03 -0400

    update

---
+++ data-structures/linked-list.test.js
@@ -0,0 +1,18 @@
+const LinkedList = require('./linked-list.js');
+
+describe.skip('Linked List', () => {
+    let LL;
+
+    beforeEach(() => {
+        LL = new LinkedList()
+    });
+
+    it('should add elements', () => {
+        LL.add(1)
+
+        // const result =
+        // const expected =
+        expect(result).toEqual(expected)
+
+    })
+})
